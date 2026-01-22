ea61623e969efb3cfacfc3eae239b83a856c8928
Stefan Schmaltz <schmaltz@qomet.de>
2026-01-22 19:59:05 +0100

    IOS Anpassungen

--- buildios.sh
+++ buildios.sh
@@ -1,4 +1,15 @@
+if [ -n "$XCODE_26_PATH" ]; then
+	export DEVELOPER_DIR="$XCODE_26_PATH"
+fi
+
+XCODE_VERSION=$(xcodebuild -version | head -n 1 | awk '{print $2}')
+XCODE_MAJOR=${XCODE_VERSION%%.*}
+if [ -z "$XCODE_MAJOR" ] || [ "$XCODE_MAJOR" -lt 26 ]; then
+	echo "Fehler: Xcode 26+ erforderlich (gefunden: ${XCODE_VERSION:-unbekannt})."
+	exit 1
+fi
+
 scripts/appGMK.sh
-grunt buildFW
+grunt makeFW
 cordova build ios --device --release --buildConfig build.json
 scripts/appGMK2.sh
